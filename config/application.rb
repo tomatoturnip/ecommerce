39e30d24b184c032717250950a35d34cebb0acf8
Lydia Ho <lyd.w.ho@gmail.com>
2014-10-21 00:40:49 -0400

    Configure to raise errors if unpermitted paramaters are passed

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module Ecommerce
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Raise errors if there are unpermitted parameters
+    config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
